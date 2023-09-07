84348a81d4ea45039d0cfc55ca9bc1b115f89567
Sanduranga <136986656+Sanduranga@users.noreply.github.com>
2023-09-07 20:54:32 +0530

    Recode react routor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/pages/Home';
-import Career from './components/pages/Career';
-import Trip from './components/pages/Trip';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/pages/Home";
+import Career from "./components/pages/Career";
+import Trip from "./components/pages/Trip";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    
     <div>
-      <Navbar/>
-      <BrowserRouter>
-        <Routes>
-          <Route index element = {<Home/>}/>
-          <Route exact path="/" element = {<Home/>} />
-          <Route path="/career" element = {<Career/>} />
-          <Route path='/plan_your_trip' element = {<Trip/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/plan_your_trip" element={<Trip />}></Route>
+      </Routes>
     </div>
-   
-  )
+  );
 }
 
 export default App;
